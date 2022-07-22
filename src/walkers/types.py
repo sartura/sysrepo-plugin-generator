bd137caf93ca986d1f496d71e12577b23d75a723
Mateo Cindric <mateo.cindric@sartura.hr>
2022-07-22 17:08:24 +0200

    Add leaf-list generation support

--- src/walkers/types.py
+++ src/walkers/types.py
@@ -118,7 +118,70 @@ class Walker(TreeWalker):
                 # previous value has to be a struct of some kind
                 self.ctx.typedef_map[struct_name].vars.append(
                     VarDef(node.type().basename(), to_c_variable(node.name()), "var"))
+        elif node.nodetype() == LyNode.LEAFLIST:
+            struct_name = to_c_variable(
+                "{}_{}".format(full_prefix, node.name()))
+            var_name = to_c_variable(node.name())
 
+            # element struct
+            element_name = to_c_variable("{}_element".format(struct_name))
+            element_var_name = var_name
+
+            print("struct {}:".format(struct_name))
+
+            # element
+            element_td = Typedef("struct", element_name)
+            element_sd = StructDef(element_name)
+
+            # data
+            data_td = Typedef("struct", struct_name)
+            data_sd = StructDef(struct_name)
+
+            # add struct variables - data element + pointer to the next node
+            element_sd.vars.append(
+                VarDef(data_td.typedef, element_var_name, "var"))
+            element_sd.vars.append(
+                VarDef(element_td.typedef + "*", "next", "var"))
+
+            self.ctx.typedefs.append(element_td)
+            self.ctx.structs.append(element_sd)
+
+            # add to typedef map
+            self.ctx.typedef_map[element_name] = element_sd
+
+            self.ctx.typedefs.append(data_td)
+            self.ctx.structs.append(data_sd)
+
+            self.ctx.typedef_map[struct_name] = data_sd
+
+            # add data variable to the data struct
+            if node.type().basename() == "enumeration":
+                # add enum type
+                enum_name = to_c_variable(
+                    "{}_{}".format(full_prefix, node.name()))
+
+                enum_ed = EnumDef(enum_name, [to_c_variable("{}_{}".format(
+                    enum_name, str(e))) for e in node.type().enums()])
+                enum_td = Typedef("enum", enum_name)
+
+                self.ctx.enums.append(enum_ed)
+                self.ctx.typedefs.append(enum_td)
+
+                self.ctx.typedef_map[enum_name] = enum_ed
+
+                # previous value has to be a struct of some kind
+                self.ctx.typedef_map[struct_name].vars.append(
+                    VarDef(enum_td.typedef, to_c_variable(node.name()), "enum"))
+            else:
+                # previous value has to be a struct of some kind
+                self.ctx.typedef_map[struct_name].vars.append(
+                    VarDef(node.type().basename(), to_c_variable(node.name()), "var"))
+
+            # add to parent struct
+            parent = to_c_variable(full_prefix)
+            if parent in self.ctx.typedef_map:
+                self.ctx.typedef_map[parent].vars.append(
+                    VarDef(element_td.typedef + "*", var_name, "var"))
         elif node.nodetype() == LyNode.LIST:
             self.ctx.prefix_stack[depth +
                                   1] = "{}_{}".format(full_prefix, node.name())
