e5d91be84b283977dc0d56e412e6356b0487f666
Mateo Cindric <mateo.cindric@sartura.hr>
2022-08-11 15:46:37 +0200

    Disable generation of obsolete operational callbacks

--- src/walkers/subscription/operational.py
+++ src/walkers/subscription/operational.py
@@ -37,7 +37,7 @@ class Walker(TreeWalker):
         return False
 
     def add_node(self, node):
-        return not node.nodetype() == LyNode.RPC and not node.nodetype() == LyNode.ACTION and not node.nodetype() == LyNode.NOTIF
+        return not node.nodetype() == LyNode.RPC and not node.nodetype() == LyNode.ACTION and not node.nodetype() == LyNode.NOTIF and node.status() is not 'obsolete'
 
     def get_callbacks(self):
         return list(reversed(self.ctx.callbacks))
