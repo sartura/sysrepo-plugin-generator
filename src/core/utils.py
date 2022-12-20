def0d8511f8870a4b78853c9091b3cf6a30beb40
Mateo Cindric <mateocindricdev@gmail.com>
2022-12-20 17:16:39 +0000

    Remove unused files

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,4 +1,20 @@
 from libyang.schema import Node as LyNode
+import os
+
+
+class CLibrary:
+    def __init__(self, outdir, name):
+        deps_dir = os.path.join(outdir, "deps")
+        self.lib_dir = os.path.join(deps_dir, name)
+
+    def generate_directories(self):
+        pass
+
+    def generate_files(self):
+        pass
+
+    def get_include_dirs(self):
+        pass
 
 
 class Callback:
