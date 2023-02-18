1a0d3f10818f7ed6c1ee5c5f57c5b69eb696a80e
Mateo Cindric <mateocindricdev@gmail.com>
2023-02-18 19:51:35 +0000

    cpp-generator: setup logging

--- src/target/cpp/generator.py
+++ src/target/cpp/generator.py
@@ -1,12 +1,43 @@
+import logging
+
 from typing import Dict, Any
 
 from core.config import GeneratorConfiguration
 from core.generator import Generator
 
+from core.log.filters import DebugLevelFilter, InfoLevelFilter
+
 
 class CPPGenerator(Generator):
+    # logger
+    logger: logging.Logger
+
     def __init__(self, yang_dir: str, out_dir: str, config: GeneratorConfiguration):
         super().__init__(yang_dir, out_dir, config)
+
+        # setup logger for the generator
+        self.logger = logging.getLogger("CPPGenerator")
+        self.logger.setLevel(logging.DEBUG)
+
+        # Debug level handler
+        debug_handler = logging.StreamHandler()
+        debug_handler.setLevel(logging.DEBUG)
+        debug_formatter = logging.Formatter(
+            '[%(levelname)s][%(name)s][%(pathname)s:%(lineno)s]: %(message)s')
+        debug_handler.setFormatter(debug_formatter)
+        debug_handler.addFilter(DebugLevelFilter())
+        self.logger.addHandler(debug_handler)
+
+        # Info level handler
+        info_handler = logging.StreamHandler()
+        info_handler.setLevel(logging.INFO)
+        info_formatter = logging.Formatter(
+            '[%(levelname)s][%(name)s]: %(message)s')
+        info_handler.setFormatter(info_formatter)
+        info_handler.addFilter(InfoLevelFilter())
+        self.logger.addHandler(info_handler)
+
+        self.logger.info("Starting C++ generator")
 
     def generate_directories(self):
         pass
